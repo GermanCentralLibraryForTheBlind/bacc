0a1cf6d25559bbe83ac133c317ccb880c8bb9394
Lars Voigt <lars.voigt@dzb.de>
2018-03-23 09:25:42 +0100

    - fixed ace localisation

--- backend/report/only-dev.js
+++ backend/report/only-dev.js
@@ -1,5 +1,5 @@
 const ReportModeler = require('./ReportModeler');
-const r = new ReportModeler('/home/alan/workspace/bacc/uploads/1342bbf8-bf39-48a3-8a44-92763d005eb4', 'de');
+const r = new ReportModeler('/home/alan/workspace/bacc/uploads/0ada3cd3-1adf-4bba-91d1-94725a198ec7', 'de');
 console.log('start report build ...');
 r.build();
 console.log('build ready');
