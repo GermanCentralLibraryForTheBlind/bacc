1f2c93ee8bac4454effa9ecaebde1a369d5759ed
Lars Voigt <lars.voigt@dzb.de>
2018-08-31 13:25:47 +0200

    - added issue count marker to tab "violations" - fixed some bugs in metadata view

--- backend/report/only-dev.js
+++ backend/report/only-dev.js
@@ -1,5 +1,5 @@
 const ReportModeler = require('./ReportModeler');
-const r = new ReportModeler('/home/alan/workspace/bacc/uploads/22a01146-02f2-46a7-8cfb-54375a0e85d8', 'de');
+const r = new ReportModeler('/home/alan/workspace/bacc/uploads/2aa09635-47fb-417d-b4d2-9132d521b8df', 'de');
 console.log('start report build ...');
 r.build();
 console.log('build ready');
