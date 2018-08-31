a1bd5f4f68e100dcb11e3aa9ae6284a032b1f527
Lars Voigt <lars.voigt@dzb.de>
2018-08-31 15:16:58 +0200

    - cosmetics

--- backend/report/only-dev.js
+++ backend/report/only-dev.js
@@ -3,7 +3,7 @@ const meow = require('meow');
 
 const cli = meow(``, {});
 
-const path = cli.input[0] || '/home/alan/workspace/bacc/uploads/fd24cc42-c8b5-43b3-a1d0-b4c600fdc913';
+const path = cli.input[0] || '/home/alan/workspace/bacc/uploads/e164c4f2-a73e-4fd3-83b6-0b45ba853f66';
 
 const r = new ReportModeler(path, 'de');
 console.log('start report build ...');
