197ad9a988e76f2a6b6c1ef7d3de2a69a9de77ca
Lars Voigt <lars.voigt@dzb.de>
2018-09-21 16:01:40 +0200

    - added tooltips when more information were needed

--- backend/report/only-dev.js
+++ backend/report/only-dev.js
@@ -3,7 +3,7 @@ const meow = require('meow');
 
 const cli = meow(``, {});
 
-const path = cli.input[0] || '/home/alan/workspace/bacc/uploads/e164c4f2-a73e-4fd3-83b6-0b45ba853f66';
+const path = cli.input[0] || '/home/alan/workspace/bacc/uploads/c2e04318-97eb-4284-bf8c-87d300d57d7a';
 
 const r = new ReportModeler(path, 'de');
 console.log('start report build ...');
