56fce23fb9b67c10f5ba3af62947f805547a6372
Lars Voigt <lars.voigt@dzb.de>
2019-01-16 14:43:05 +0100

    - added new conditions to hint section-missing

--- backend/hints/viaAxe/checks/section-missing.js
+++ backend/hints/viaAxe/checks/section-missing.js
@@ -1,1 +1,4 @@
-return !!node.querySelector('section');
+return !!node.querySelector('section') ||
+  !!node.querySelector('nav') ||
+  !!node.querySelector('article') ||
+  !!node.querySelector('aside');
