cb871cb19c698741fad8960e741482d947be7dcc
Lars Voigt <lars.voigt@dzb.de>
2019-02-12 16:14:14 +0100

    - added new hint "duplicate title"

---
+++ backend/hints/viaAce/duplicate-title.js
@@ -0,0 +1,21 @@
+'use strict';
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
+// check
+
+module.exports = epub => {
+
+  const titles = [];
+  epub.contentDocs.forEach((cDoc) => {
+
+    const content = fs.readFileSync(cDoc.filepath).toString();
+    const doc = new DOMParser().parseFromString(content);
+    const title = doc.getElementsByTagName("title");
+
+    if (title[0])
+      titles.push(title[0].childNodes[0].nodeValue);
+  });
+
+  // unique titles count vs real titles count
+  return [...new Set(titles)].length !== titles.length;
+};
