da1407acccb3f08f7e76bf0323582f1e793dc3f9
Lars Voigt <lars.voigt@dzb.de>
2019-02-15 13:33:59 +0100

    - fixed hint

--- backend/hints/viaAce/nav-landmarks-missing.js
+++ backend/hints/viaAce/nav-landmarks-missing.js
@@ -6,6 +6,11 @@ const DOMParser = require('xmldom').DOMParser;
 
 // check
 module.exports = epub => {
+
+  // fallback epub2
+  if(Object.keys(epub.navDoc).length === 0)
+    return false;
+
   const navDocFullPath = path.join(epub.basedir, epub.navDoc.src);
   const content = fs.readFileSync(navDocFullPath).toString();
   const doc = new DOMParser().parseFromString(content);
